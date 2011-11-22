20f51002c93e01e202ab9de790a1c96f110bae8f
Anthony Patterson <a.patterson@ucc.ie>
2011-11-22 16:22:33 +0000

    essentials links

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject clad "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.3.0"]
+                           [enlive "1.0.0"]
                            [noir "1.2.1"]]
             :main clad.server)
 
--- src/clad/views/welcome.clj
+++ src/clad/views/welcome.clj
@@ -2,8 +2,23 @@
   (:require [clad.views.common :as common]
             [noir.content.getting-started])
   (:use [noir.core :only [defpage]]
-        [hiccup.core :only [html]]))
+        [hiccup.core :only [html]]
+	[net.cgrand.enlive-html]))
 
-(defpage "/welcome" []
-         (common/layout
-           [:p "Welcome to clad"]))
+(def essential (html-resource "clad/views/more.html"))
+
+(deftemplate clad "clad/views/CLAD_1.html"
+  [file]
+
+  [:#essential]
+  (clone-for [snip (select essential [:.more])]
+   [:li]
+   (content {:tag :a
+             :attrs {:href (str "/clad/" (apply str (emit* (:id (:attrs snip)))))}
+             :content (apply str (emit* (:id (:attrs snip))))}))
+
+  [:.CF2]
+  (content (select essential [(keyword (str "#" file))])))
+
+(defpage "/clad" [] (clad "whatis"))
+(defpage [:get ["/clad/:more" :more #".+"]] {:keys [more]} (clad more))
