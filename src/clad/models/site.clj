f86f11a3eabb15f58623e9b9c6a61facfba036cf
Anthony Patterson <a.patterson@ucc.ie>
2012-01-03 16:18:52 +0000

    usmoved sitemap to models

---
+++ src/clad/models/site.clj
@@ -0,0 +1,7 @@
+(ns clad.models.site)
+
+(def
+  ^{:doc "Sitemap: Links level 2 headers (:title) to html fragments (:title)"}
+  site [{:id "essentials" :title "Essentials"}
+           {:id "projections" :title "Global Projections"}
+           {:id "Impacts" :title "Irish Coasts"}])
