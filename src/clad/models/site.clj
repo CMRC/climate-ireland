13a126384683b59859ad4bb16069d7c4f5691312
Anthony Patterson <a.patterson@ucc.ie>
2012-01-04 15:56:01 +0000

    filling out structure

--- src/clad/models/site.clj
+++ src/clad/models/site.clj
@@ -8,8 +8,30 @@
            Section id
            Section Title"}
   
-  site {:climate_change
-        {:title "Climate Change"
-         :sections [{:id "essentials"  :title "Essentials"}
-                    {:id "projections" :title "Global Projections"}
-                    {:id "Impacts"     :title "Irish Coasts"}]}})
+  site (array-map
+        :home           {:title "Home"
+                         :sections [{:id "main"  :title "Home"}]}
+        :climate_change {:title "Climate Change"
+                         :sections [{:id "essentials"  :title "Essentials"}
+                                    {:id "projections" :title "Global Projections"}
+                                    {:id "Impacts"     :title "Irish Coasts"}]}
+        :adaptation     {:title "Adaptation"
+                         :sections [{:id "whyadapt"    :title "Why Climate Adaptation?"}
+                                    {:id "adaptcom"    :title "Adaptive Co-Management"}]}
+        :tools_methods  {:title "Tools & Methods"
+                         :sections [{:id "tm"          :title "Tools & Methods"}]}
+        :policy_law     {:title "Policy & Law"
+                         :sections [{:id "howag"       :title "How Adaptation Governance Works"}
+                                    {:id "eu"          :title "European Union"}
+                                    {:id "ireland"     :title "Ireland"}
+                                    {:id "regions"     :title "Regions & Communities"}]}
+        :case_studies   {:title "Case Studies"
+                         :sections [{:id "howdotheydo" :title "How do they manage?"}
+                                    {:id "cs_ireland"  :title "Ireland"}
+                                    {:id "cs_int"      :title "International"}
+                                    {:id "issues"      :title "Look for your specific issue"}
+                                    {:id "experience"  :title "Tell us about your experience!"}]}
+        :resources      {:title "Resources"
+                         :sections [{:id "whyadapt"    :title "Why Climate Adaptation?"}]}
+        :icrn           {:title "ICRN"
+                         :sections [{:id "whyadapt"    :title "Why Climate Adaptation?"}]}))
--- src/clad/views/welcome.clj
+++ src/clad/views/welcome.clj
@@ -8,16 +8,16 @@
   (:import (java.net URLEncoder
                      URLDecoder)))
   
-(defn format-text [page]
+(defn format-text [topic page]
   (let [pre (html-resource "clad/views/more.html")
        post (transform pre [:a.Glossary] 
                            (fn [a-selected-node]
                                (assoc-in a-selected-node [:attrs :href] 
-                                 (str "/clad/" page "/glossary/" (:id (:attrs a-selected-node))))))]
+                                 (str "/clad/" page "/" topic "/glossary/" (:id (:attrs a-selected-node))))))]
   post)) 
 
 (defn make-links [page topic]
-  (clone-for [snip (select (format-text "") [(keyword (str "." topic))])]
+  (clone-for [snip (select (format-text "" page) [(keyword (str "." topic))])]
              [:li]
              (content {:tag :a
                        :attrs {:href (str "/clad/" page "/"
@@ -27,13 +27,13 @@
 (deftemplate clad "clad/views/CLAD_1.html"
   [{link :link glossary :glossary page :page}]
 
-  [:#buttons]
+  [:#buttons :li]
   (clone-for [context sitemap/site]
-             [:li :a]
+             [:a]
              (fn [a-selected-node] 
                (assoc-in a-selected-node [:content]
                          (:title (val context))))
-             [:li :a]
+             [:a]
              (fn [a-selected-node] 
                (assoc-in a-selected-node [:attrs :href]
                          (str "/clad/" (name (key context))))))
@@ -46,16 +46,16 @@
              (make-links page (:id header)))
 
   [:#Glossary]
-  (content (select (format-text link) [[:.Glossary (keyword (str "#" (URLDecoder/decode glossary)))]]))
+  (content (select (format-text link page) [[:.Glossary (keyword (str "#" (URLDecoder/decode glossary)))]]))
   
   [:#Main_Text]
-  (content (select (format-text link) [(keyword (str "#" (URLDecoder/decode link))) :.Main_Text]))
+  (content (select (format-text link page) [(keyword (str "#" (URLDecoder/decode link))) :.Main_Text]))
   
   [:#Key_Text]
-  (content (select (format-text link) [(keyword (str "#" (URLDecoder/decode link))) :.Key_Text]))
+  (content (select (format-text link page) [(keyword (str "#" (URLDecoder/decode link))) :.Key_Text]))
   
   [:#Picture]
-  (content (select (format-text link) [(keyword (str "#" (URLDecoder/decode link))) :.Picture])))
+  (content (select (format-text link page) [(keyword (str "#" (URLDecoder/decode link))) :.Picture])))
 
 (defpage "/clad" [] (clad {:link "whatis" :glossary "climate" :page "climate_change"}))
 (defpage "/clad/:page" {:keys [page]} (clad {:link "whatis" :glossary "climate" :page page}))
