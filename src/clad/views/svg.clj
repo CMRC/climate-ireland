6b83cd977871d6c3c76cd17c686fef0e3942c822
Anthony Patterson <a.patterson@ucc.ie>
2012-02-21 13:06:36 +0000

    quartiles

--- src/clad/views/svg.clj
+++ src/clad/views/svg.clj
@@ -3,18 +3,25 @@
 			     parse-xml transform-xml filter-xml]]
 	analemma.svg
         clad.models.gdal
+        incanter.stats
 	[clojure.java.io :only [file]]))
+
+(def run "temp2020djf")
 
 (def counties-svg (parse-xml (slurp "src/clad/views/counties.svg")))
 
+(def quartiles (quantile (map #(bycounty-memo % run) counties)))
+
 (defn counties-map []
-  (let [run "temp2020djf"]
-    {:status 200
-     :headers {"Content-Type" "image/svg+xml"}
-     :body
-     (emit (reduce #(transform-xml %1
-                                   [{:id %2}]
-                                   (fn [elem] (add-style elem :fill (if (> (bycounty-memo %2 run) 8) "#5555ff" "#ff5555"))))
-                   counties-svg
-                   counties))}))
+  {:status 200
+   :headers {"Content-Type" "image/svg+xml"}
+   :body
+   (emit (reduce #(transform-xml %1
+                                 [{:id %2}]
+                                 (fn [elem] (add-style elem :fill (cond (< (bycounty-memo %2 run) (nth quartiles 1)) "#319"
+                                                                        (< (bycounty-memo %2 run) (nth quartiles 2)) "#517"
+                                                                        (< (bycounty-memo %2 run) (nth quartiles 3)) "#715"
+                                                                        :else "#913"))))
+                 counties-svg
+                 counties))})
                    
