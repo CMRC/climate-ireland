03a5668d51b68529667e29f70a19b9d80202743f
Anthony Patterson <a.patterson@ucc.ie>
2012-03-12 14:44:11 +0000

    time series plot of model data

--- src/clad/views/charts.clj
+++ src/clad/views/charts.clj
@@ -1,6 +1,23 @@
 (ns clad.views.charts
   (:use incanter.core, incanter.stats, incanter.charts,
-	incanter.io incanter.processing, clad.models.gdal))
+	incanter.io clad.models.couch)
+  (:require [clj-time.core :as time]
+            [clj-time.coerce :as coerce])
+  (:import (java.io ByteArrayOutputStream
+                    ByteArrayInputStream)
+           (java.lang Integer)))
 
-
+(defn plot-models []
+  (let [model (get-models)
+        x (map #(coerce/to-long (time/date-time (Integer/parseInt (:key %)))) model)
+        y (map #(:datum.value (:value %)) model)
+        chart (doto (time-series-plot x y :y-label "%"))
+        out-stream (ByteArrayOutputStream.)
+        in-stream (do
+                    (save chart out-stream)
+                    (ByteArrayInputStream. 
+                     (.toByteArray out-stream)))]
+    {:status 200
+     :headers {"Content-Type" "image/png"}
+     :body in-stream}))
 
