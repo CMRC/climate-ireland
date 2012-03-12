0b7e4ed39321700adf4fbf40c9bd6b80723b765b
Anthony Patterson <a.patterson@ucc.ie>
2012-03-12 14:44:49 +0000

    time series

--- src/clad/models/couch.clj
+++ src/clad/models/couch.clj
@@ -6,10 +6,19 @@
 
 (defn get-run-data [year months]
   (clutch/with-db "icip"
-    (clutch/save-view "vals"
-                      (clutch/view-server-fns
-                       :clojure
-                       {:by-ym
-                        {:map (fn [doc] [[[(:year doc) (:months doc)]
+    #_(clutch/save-view "vals"
+                        (clutch/view-server-fns
+                         :clojure
+                         {:by-ym
+                          {:map (fn [doc] [[[(:year doc) (:months doc)]
                                             doc]])}}))
     (map #(:value %) (clutch/get-view "vals" :by-ym {:key [year months]}))))
+
+(defn get-models []
+  (clutch/with-db "icip"
+    (clutch/save-view "models"
+                      (clutch/view-server-fns
+                       :clojure
+                       {:by-model
+                        {:map (fn [doc] [[(:year doc),doc]])}}))
+    (clutch/get-view "models" :by-model)))
