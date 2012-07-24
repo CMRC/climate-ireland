8fe683285452c3dfbdf5af67d745df415dd9657c
Anthony Patterson <a.patterson@ucc.ie>
2012-07-24 17:29:13 +0200

    password protection

--- src/clad/pw.clj
+++
@@ -1,7 +0,0 @@
-(ns clad.pw
-  (:require (cemerick.friend [credentials :as creds])))
-  
-(def users {"guest" {:username "guest"
-                     :password (creds/hash-bcrypt "climate")
-                     :roles #{::user}}})
-
