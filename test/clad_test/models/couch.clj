150de8526c3e71c0c9c1dba95177e1cdc4f37df3
Anthony Patterson <a.patterson@ucc.ie>
2012-07-31 12:18:24 +0200

    removed broken test

--- test/clad_test/models/couch.clj
+++ test/clad_test/models/couch.clj
@@ -1,7 +1,7 @@
 (ns clad-test.models.couch
   (:use clojure.test))
 
-(defn test-temp-diff []
+#_(defn test-temp-diff []
   (let [ref 273.15
         proj 290.12]
     (is (temp-diff ref proj) (- proj ref))))
