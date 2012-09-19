0c4f689349878e68f41e01dd81e8df256dc9b30e
Anthony Patterson <a.patterson@ucc.ie>
2012-09-19 12:50:16 +0200

    calculating yearly averages

--- src/clad/models/decades.R
+++ src/clad/models/decades.R
@@ -5,6 +5,9 @@ base.path <- "temp/"
 cdoseasonaldecadal <- function(run, decade) {
   system(paste("cd " ,base.path, ";cdo splitseas -yseasmean -selyear,", decade, " ", run, ".nc ", run, sep=""))
 }
+cdoyearlydecadal <- function(run, decade) {
+  system(paste("cd " ,base.path, ";cdo yearmean -selyear,", decade, " ", run, ".nc ", run, "J2D", ".nc", sep=""))
+}
 maketifseasonaldecadal <- function(run, decade, season, variable) {
   system(paste("cd ",base.path, ";gdal_translate -a_ullr -13.3893 56.3125 -3.39428 50.4016 \"NETCDF:", run,
                season, ".nc:", variable, "\" seas.tif",sep=""))
@@ -13,8 +16,9 @@ maketifseasonaldecadal <- function(run, decade, season, variable) {
 
 for(start in c(2021L,2031L,2041L,2051L)) {
   for(run in c("MM_caCLM4_A1B_4km","MM_caCLM4_A2_4km","MM_haCLM4_RCP45_4km","MM_haCLM4_RCP85_4km")) {
+    cdoyearlydecadal(run, paste(toString(start),sep="/",toString(start+9)))
     cdoseasonaldecadal(run, paste(toString(start),sep="/",toString(start+9)))
-    for(season in c("DJF","MAM","JJA","SON")) {
+    for(season in c("DJF","MAM","JJA","SON","J2D")) {
       for(var in c("lat","lon","PS","TOT_PREC","PMSL","QV_2M","T_2M","RUNOFF_G","RUNOFF_S","TMAX_2M","TMIN_2M","VGUST_DYN")) {
         seasstr <- paste(toString(start),sep="",toString(start+9-2000))
         sd20s <- maketifseasonaldecadal(run, seasstr, season, var) 
