0c3c147c34211abb81ccf3937bd4657932b0769a
Barry <b.odwyer@ucc.ie>
2012-12-04 08:50:22 +0000

    Modified css to omit news bar etc.

--- src/clad/views/welcome.clj
+++ src/clad/views/welcome.clj
@@ -432,6 +432,9 @@
 
 (defpage "/ci/about" []
   (two-pane "clad/views/CI_About.html" "about" {:tag :object :attrs {:data "/img/impact-tool.svg"}}))
+  
+(defpage "/ci/about/impacts" []
+  (two-pane "clad/views/CI_Impacts.html" "about" {:tag :object :attrs {:data "/img/impact-tool.svg"}}))
 
 (defpage "/ci/climate-change/:tab" {:keys [tab]}
   (one-pane "clad/views/CI_ClimateChange.html" "climate-change" tab))
