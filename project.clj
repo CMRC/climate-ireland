e225263f15894af353976c9bb0af3441272adb36
Anthony Patterson <a.patterson@ucc.ie>
2013-01-25 17:17:26 +0100

    update friend

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                            [com.ashafa/clutch "0.4.0-RC1"]
 			   [org.clojars.pallix/batik "1.7.0"]
                            [clj-logging-config "1.9.7"]
-                           [com.cemerick/friend "0.0.9"]
+                           [com.cemerick/friend "0.1.3"]
                            [org.clojure/clojurescript "0.0-1011"]
                            [com.keminglabs/c2 "0.2.1"]
                            [com.keminglabs/vomnibus "0.3.1"]]
