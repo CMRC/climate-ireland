0eece9d7d46b94d44038afeae7e0bd9daed207d6
Anthony Patterson <a.patterson@ucc.ie>
2013-01-31 15:59:19 +0000

    new ordering of scenarios

--- src/clad/views/welcome.clj
+++ src/clad/views/welcome.clj
@@ -102,14 +102,14 @@
 
 
 (def seasons {"DJF" "Winter", "MAM" "Spring", "JJA" "Summer", "SON" "Autumn" "J2D" "All Seasons"})
-(def scenarios {["CGCM31" "A1B"] "A1B"
+(def scenarios (array-map
+		["CGCM31" "A1B"] "A1B"
                 ["CGCM31" "A2"] "A2 [1]"
                 ["HadGEM" "RCP45"] "RCP45"
                 ["HadGEM" "RCP85"] "RCP85"
                 ["ICARUS" "a2"] "A2 [2]"
                 ["ICARUS" "b2"] "B2"
-                ["ICARUS" "ICARUS"] "A2 + B2 ensemble"
-                ["ensemble" "ensemble"] "ensemble"})
+                ["ensemble" "ensemble"] "ensemble"))
                 
 (defsnippet map-help "clad/views/chart-help.html"
   [:#map-help]
