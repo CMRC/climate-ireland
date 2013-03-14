3507e70485608baf3649f127b24f3109ba914322
Anthony Patterson <a.patterson@ucc.ie>
2013-03-14 17:25:40 +0100

    mismatched braces

--- src/clad/views/questions.clj
+++ src/clad/views/questions.clj
@@ -25,21 +25,21 @@
                 "Dissatisfied"
                 "Unsure"
                 "Satisfied"
-                "Very satisfied"]}}
+                "Very satisfied"]}
    "Navigation"
    {:question "How easy was it to navigate within the website?"
     :responses ["Very difficult"
                 "Difficult"
                 "Neutral"
                 "Easy"
-                "Very easy"]}}
+                "Very easy"]}
    "Organisation"
    {:question "How well was the information organised within the site?"
     :responses ["Very disorganised"
                 "Disorganised"
                 "Unsure"
                 "Well organised"
-                "Very well organised"]}}
+                "Very well organised"]}
    "Quality"
    {:question "How was the overall quality of the website content?"
     :responses ["Poor"
